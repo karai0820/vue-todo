3665c4bc52be98392eb70973bdfd783942307b7f
Kentaro Arai <k.arai0820@gmail.com>
2019-04-30 02:06:13 +0900

    make todolist

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>TODO
+ <span calss="info">({{remaining.length}}/{{todos.length}})</span>
+  </h1>
+
+  <ul>
+  <ToDoItem v-for="todo as todos" :todo="todo" @delete="deleteTodo">
+  
+  </ul>
+  <form @submit.prevent="addTodo"> 
+  <input type="text" v-model="newTodo">
+  <input type="submit" value="Add">
+  </form>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from "./components/ToDoItem.vue"
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+name:"app",
+components:{
+	ToDoItem
+},
+date(){
+	return{
+	todos:[
+	{id:1,text:"abc",isDone:faulse},
+	{id:2,text:"dcr",isDone:faulse}
+],
+newToDo:""
+	}
+},
+methods:{
+addTodo:function(){
+	const newId = Math.max.apply(null,this.todos.map(t=>t.id))+1;
+	this.todos.push({id:newId,text:this.newTodo,isDone:false});
+	this.newTodo="";
+	},
+deleteTodo:function(todo){
+	this.todos=this.todos.filter(item=>item!==todo);
+}	
+},
+computed:{
+	remaining:function(){
+	return this.todos.filter(function(todo){
+	return !todo.isDone;
+
+	});
+	}
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
